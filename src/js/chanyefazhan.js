8bb61ef6a48017829c9aacb11e2b6b432adbc9c8
yanglinsheng <2533079308@qq.com>
2018-10-11 11:48:29 +0800

    2018.10.11

--- src/js/chanyefazhan.js
+++ src/js/chanyefazhan.js
@@ -2,11 +2,11 @@ $(function() {
     var btns = $('.nav .row a'),
         tabsDiv = $('body>.tab-content>div');
     btns.eq(0).addClass('active');
-    tabsDiv.eq(0).addClass('show');
+    // tabsDiv.eq(0).addClass('show');
     btns.on('click', function() {
         var _index = btns.index($(this));
         $(this).addClass('active').siblings().removeClass('active');
-        tabsDiv.eq(_index).addClass('show').siblings().removeClass('show');
+        // tabsDiv.eq(_index).addClass('show').siblings().removeClass('show');
 
     })
 })
